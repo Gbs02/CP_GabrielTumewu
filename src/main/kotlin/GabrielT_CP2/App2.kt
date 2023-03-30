13467816cf647a441f562f6879fd2be301e070d2
Gabs <tumewujibril@gmail.com>
2023-03-30 09:55:22 +0800

    CP3 susah :)

--- src/main/kotlin/GabrielT_CP2/App2.kt
+++ src/main/kotlin/GabrielT_CP2/App2.kt
@@ -1,12 +1,17 @@
 package GabrielT_CP2
 
 fun main() {
+do {
     println("Coba Masukkan anggka:")
     val number = readLine()?.toInt() ?: 0
 
     if (number % 11 == 0) {
         println("$number adalah kelipatan 11")
-    } else {
+    }else if (number == 0){
+        break
+    }
+    else {
         println("$number bukan kelipatan  11")
     }
+}while (number !=0)
 }
